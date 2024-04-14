2d538e2957dc07030a8d4425c1e0e3c9163a6b2c
CYLEE <853051095@QQ.COM>
2024-04-14 21:29:09 +0800

    优化颜色生成器

--- ui/src/components/detail/detail.vue
+++ ui/src/components/detail/detail.vue
@@ -47,7 +47,7 @@
   <main style="margin-top: 1vh;display: flex;align-items: center;justify-content: center">
     <!-- 左边的列表 -->
     <div v-if="isControlInfoGet" ref="detailListContainer" style="width: auto">
-      <el-scrollbar style="display: flex;" >
+      <el-scrollbar style="display: flex;">
         <VueDraggable
             v-model="detailList"
             :animation="150"
@@ -60,10 +60,10 @@
           <div :id="item.detail_id" v-for="item in detailList" :key="item.detail_id"
                class="detail-item ">
             <div v-if="isEditMod" class="sort_detail_key_list"
-                style="width: 95%;height: 20px;display: flex;border-radius: 2px;align-content: center;justify-content: center;background: #13ce66">
-                <el-icon size="16" style="height: 100%">
-                  <Sort/>
-                </el-icon>
+                 style="width: 95%;height: 20px;display: flex;border-radius: 2px;align-content: center;justify-content: center;background: #13ce66">
+              <el-icon size="16" style="height: 100%">
+                <Sort/>
+              </el-icon>
             </div>
             <div class="" :style="KeySizeStyle()">
               <div style=" width:100%;height:100%;display: flex;flex-direction: column">
@@ -216,7 +216,7 @@
              width="90vw">
     <div
         style="width: 100%;display: flex;flex-direction: column;align-items: center;justify-content: center;margin-top: 30px;">
-      <el-form label-position="right" label-width="auto" >
+      <el-form label-position="right" label-width="auto">
         <el-form-item label="名称">
           <el-input v-model="editItem.detail_name"></el-input>
         </el-form-item>
@@ -226,8 +226,8 @@
                              popper-class="hex"
                              v-model="editItem.detail_color" show-alpha :predefine="predefineColors"/>
             <div style="display: flex;flex-grow: 1;margin-left: 5px;flex-wrap: wrap">
-              <el-button style="margin-left: 1px" :style="{background:item}" size="small"
-                  v-for="item in predefineColorListAuto" @click="editItem.detail_color=item"></el-button>
+              <el-button style="margin: 1px" :style="{background:item}" size="small"
+                         v-for="item in predefineColorListAuto" @click="editItem.detail_color=item"></el-button>
 
             </div>
           </div>
@@ -373,155 +373,8 @@ const isControlInfoGet = ref(false)
 const tk = ref(0)
 const lastColor = ref('')
 
-const predefineColorList = [
-  '#00c8c8',
-  '#FFFF00',
-  '#0000FF',
-  '#008000',
-  '#FFA500',
-  '#4B0082',
-  '#800080',
-  '#F0F8FF',
-  '#FAEBD7',
-  '#00FFFF',
-  '#7FFFD4',
-  '#F0FFFF',
-  '#F5F5DC',
-  '#FFE4C4',
-  '#000000',
-  '#FFEBCD',
-  '#8A2BE2',
-  '#A52A2A',
-  '#DEB887',
-  '#5F9EA0',
-  '#7FFF00',
-  '#D2691E',
-  '#FF7F50',
-  '#6495ED',
-  '#FFF8DC',
-  '#DC143C',
-  '#00FFFF',
-  '#00008B',
-  '#008B8B',
-  '#B8860B',
-  '#A9A9A9',
-  '#006400',
-  '#BDB76B',
-  '#8B008B',
-  '#556B2F',
-  '#FF8C00',
-  '#9932CC',
-  '#8B0000',
-  '#E9967A',
-  '#8FBC8F',
-  '#483D8B',
-  '#2F4F4F',
-  '#00CED1',
-  '#9400D3',
-  '#FF1493',
-  '#00BFFF',
-  '#696969',
-  '#1E90FF',
-  '#D19275',
-  '#B22222',
-  '#FFFAF0',
-  '#228B22',
-  '#FF00FF',
-  '#DCDCDC',
-  '#F8F8FF',
-  '#FFD700',
-  '#DAA520',
-  '#808080',
-  '#ADFF2F',
-  '#F0FFF0',
-  '#FF69B4',
-  '#CD5C5C',
-  '#FFFFF0',
-  '#F0E68C',
-  '#E6E6FA',
-  '#FFF0F5',
-  '#7CFC00',
-  '#FFFACD',
-  '#ADD8E6',
-  '#F08080',
-  '#E0FFFF',
-  '#FAFAD2',
-  '#D3D3D3',
-  '#90EE90',
-  '#FFB6C1',
-  '#FFA07A',
-  '#20B2AA',
-  '#87CEFA',
-  '#8470FF',
-  '#778899',
-  '#B0C4DE',
-  '#FFFFE0',
-  '#00FF00',
-  '#32CD32',
-  '#FAF0E6',
-  '#FF00FF',
-  '#800000',
-  '#66CDAA',
-  '#0000CD',
-  '#BA55D3',
-  '#9370D8',
-  '#3CB371',
-  '#7B68EE',
-  '#00FA9A',
-  '#48D1CC',
-  '#C71585',
-  '#191970',
-  '#F5FFFA',
-  '#FFE4E1',
-  '#FFE4B5',
-  '#FFDEAD',
-  '#000080',
-  '#FDF5E6',
-  '#808000',
-  '#6B8E23',
-  '#FF4500',
-  '#DA70D6',
-  '#EEE8AA',
-  '#98FB98',
-  '#AFEEEE',
-  '#D87093',
-  '#FFEFD5',
-  '#FFDAB9',
-  '#CD853F',
-  '#FFC0CB',
-  '#DDA0DD',
-  '#B0E0E6',
-  '#BC8F8F',
-  '#4169E1',
-  '#8B4513',
-  '#FA8072',
-  '#F4A460',
-  '#2E8B57',
-  '#FFF5EE',
-  '#A0522D',
-  '#C0C0C0',
-  '#87CEEB',
-  '#6A5ACD',
-  '#708090',
-  '#FFFAFA',
-  '#00FF7F',
-  '#4682B4',
-  '#D2B48C',
-  '#008080',
-  '#D8BFD8',
-  '#FF6347',
-  '#40E0D0',
-  '#EE82EE',
-  '#D02090',
-  '#F5DEB3',
-  '#FFFFFF',
-  '#F5F5F5',
-  '#9ACD32',
-]
 
-const predefineColors = ref([
-
-])
+const predefineColors = ref([])
 
 
 const GetControlTypeStyle = (tp: number) => {
@@ -544,7 +397,6 @@ const GetControlTypeStyle = (tp: number) => {
     case ControlType.RunExe:
       sty.background = "#2BDAB1"
       break
-
     case ControlType.RunCmd:
       sty.background = "#c8c9cc"
       break
@@ -587,42 +439,92 @@ onUnmounted(
     }
 )
 
-const GetColorByInt= (num:number)  =>{
-  let b = Math.floor(num%255)
-  num = num/255
-  let g = Math.floor(num%255)
-  num = num/255
-  let r = Math.floor(num%255)
-  console.log(r,g,b)
-  return 'rgb('+r+','+g+','+ b+')'
+const GetColorByInt = (num: number) => {
+  let b = Math.floor(num % 256)
+  num = num / 256
+  let g = Math.floor(num % 256)
+  num = num / 256
+  let r = Math.floor(num % 256)
+  return 'rgb(' + r + ',' + g + ',' + b + ')'
 }
-const GetAutoColor=(num:number)=>{
-  let  color = []
-  const totalColor = 255 * 255 * 255
-  const colorSpace = totalColor/num
-  for (let i = 0; i < totalColor; i=i+colorSpace) {
-    color.push(GetColorByInt(i))
+
+const GetColorByRGB = (r: number, g: number, b: number) => {
+
+  return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
+const GetAutoColor = (num: number) => {
+  let color = []
+  const colorSpace = Math.floor(256 / (num / 6)+1)
+  console.log(num)
+  console.log(colorSpace)
+  let now = "r"
+  let add = true
+  let r = 0
+  let g = 0
+  let b = 0
+  for (let i = 0; i < 6; i++) {
+    console.log(now, add)
+    for (let j = 0; j < num / 6; j++) {
+      switch (now) {
+        case 'r':
+          // brg
+          r = 255
+          if (add) {
+            g = j * colorSpace
+            b = 0
+          } else {
+            g = 0
+            b = 255 - j * colorSpace
+          }
+          break
+        case 'g':
+          // rgb
+          g = 255
+          if (add) {
+            b = j * colorSpace
+            r = 0
+          } else {
+            b = 0
+            r = 255 - j * colorSpace
+          }
+          break
+        case 'b':
+          // gbr
+          b = 255
+          if (add) {
+            r = j * colorSpace
+            g = 0
+          } else {
+            r = 0
+            g = 255 - j * colorSpace
+          }
+          break
+      }
+      console.log(r, g, b)
+      color.push(GetColorByRGB(r, g, b))
+    }
+    add = !add
+    if (!add) {
+      switch (now) {
+        case 'r':
+          now = 'g'
+          break
+        case 'g':
+          now = 'b'
+          break
+        case 'b':
+          now = 'r'
+          break
+      }
+      console.log(1112313, now)
+    }
   }
-return color
+  return color
 }
 
 
-let predefineColorListAuto =  GetAutoColor(60)
+let predefineColorListAuto = GetAutoColor(60)
 
-// const SetDetailListContainerSize = (detailWidth: number) => {
-//   if (detailListContainer.value == null || detailListContainer.value.style == null) {
-//     return [[]]
-//   }
-//   let canUse = Math.floor(window.innerWidth - window.innerWidth * 0.01)
-//   let mayCount = Math.floor(canUse / detailWidth)
-//   let realCanUse = canUse - mayCount * 2
-//   let realCount = Math.floor(realCanUse / detailWidth)
-//   let realHeight = Math.floor(window.innerHeight*0.9)
-//
-//   let realWidth = realCount * (parseInt(String(detailWidth)) + 2)
-//   detailListContainer.value.style.width = realWidth + 'px'
-//   detailListContainer.value.style.height = realHeight + 'px'
-// }
 
 const GetKeyWidth = (): number => {
   return parseInt(controlInfo.value.key_width)
@@ -632,7 +534,7 @@ const ClickEditSizeBtn = () => {
   sizeEdit.value.key_width = parseInt(controlInfo.value.key_width)
   isEditSize.value = true
 }
-const audio = new Audio('/touch_001.mp3?ts='+GetNewId()); // 替换成你音频文件的路径
+const audio = new Audio('/touch_001.mp3?ts=' + GetNewId()); // 替换成你音频文件的路径
 
 const ExecDetail = (item: ControlDetail) => {
   if ((item.control_type != ControlType.Normal) && (isEditMod.value || item.run_state == RunState.Running)) {
